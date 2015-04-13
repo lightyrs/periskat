0b6cd3004d97492bf8435deac4182f9a4da370f9
Harris Novick <harrisdavidnovick@gmail.com>
2015-04-13 00:33:06 -0400

    Meerkat Client and Populator.
    Fetch Meerkats and Broadcasters
    Populate DB with Client Responses

--- lib/clients/meerkat_client.rb
+++ lib/clients/meerkat_client.rb
@@ -7,43 +7,92 @@ class MeerkatClient
   end
 
   def broadcasts
-    @broadcast_urls = []
+    @broadcasts = []
     request = Typhoeus::Request.new(BROADCASTS_ENDPOINT, followlocation: true)
     request.on_complete do |response|
-      if response.success?
-        results = Oj.load(response.body).fetch("result", [])
-        @broadcast_urls = results.map { |result| result["broadcast"] }
-      elsif response.timed_out?
-        puts "request timed out".red
-      elsif response.code == 0
-        puts response.return_message.red
-      else
-        puts "HTTP request failed: #{response.code.to_s}"
-      end
+      handle_response(response, :summaries)
     end
     @hydra.queue(request)
     @hydra.run
-    puts @broadcast_urls.inspect.blue
+    broadcasters(@broadcasts.map { |broadcast| broadcast["result"]["broadcaster"]["id"] })
+    { broadcasts: @broadcasts, broadcasters: @broadcasters }
   end
 
   def broadcast(broadcast_id)
+    @broadcast = {}
     request = Typhoeus::Request.new(broadcast_url(broadcast_id), followlocation: true)
     request.on_complete do |response|
-      if response.success?
-        response.body
-      elsif response.timed_out?
-        puts "request timed out".red
-      elsif response.code == 0
-        puts response.return_message.red
-      else
-        puts "HTTP request failed: #{response.code.to_s}"
-      end
+      handle_response(response, :parse_object_response)
     end
     @hydra.queue(request)
     @hydra.run
+    @broadcast
+  end
+
+  def broadcasters(broadcaster_ids)
+    @broadcasters = []
+    broadcaster_ids.each do |broadcaster_id|
+      request = Typhoeus::Request.new(broadcaster_url(broadcaster_id), followlocation: true)
+      request.on_complete do |response|
+        handle_response(response, :populate_broadcasters)
+      end
+      @hydra.queue(request)
+    end
+    @hydra.run
+    @broadcasters
+  end
+
+  def broadcaster(broadcaster_id)
+    @broadcaster = {}
+    request = Typhoeus::Request.new(broadcaster_url(broadcaster_id), followlocation: true)
+    request.on_complete do |response|
+      @broadcaster = handle_response(response, :parse_object_response)
+    end
+    @hydra.queue(request)
+    @hydra.run
+    @broadcaster
   end
 
   private
+
+  def summaries(broadcasts_response)
+    Oj.load(broadcasts_response.body).fetch("result", []).each do |result|
+      request = Typhoeus::Request.new(result["broadcast"], followlocation: true)
+      request.on_complete do |response|
+        handle_response(response, :populate_broadcasts)
+      end
+      @hydra.queue(request)
+    end
+    @hydra.run
+  end
+
+  def populate_broadcasts(response)
+    @broadcasts.push(parse_object_response(response))
+  end
+
+  def populate_broadcasters(response)
+    @broadcasters.push(parse_object_response(response))
+  end
+
+  def parse_object_response(response)
+    Oj.load(response.body)
+  end
+
+  def handle_response(response, callback)
+    if response.success?
+      self.send(callback, response)
+    elsif response.timed_out?
+      puts "request timed out".red
+    elsif response.code == 0
+      puts response.return_message.red
+    else
+      puts "HTTP request failed: #{response.code.to_s}"
+    end
+  end
+
+  def broadcaster_url(broadcaster_id)
+    "https://resources.meerkatapp.co/users/#{broadcaster_id}/profile?v=2"
+  end
 
   def broadcast_url(broadcast_id)
     "http://resources.meerkatapp.co/broadcasts/#{broadcast_id}/summary"
---
+++ lib/populators/meerkat_populator.rb
@@ -0,0 +1,67 @@
+class MeerkatPopulator
+
+  def self.fetch_and_persist!
+    response_hash = MeerkatClient.new.broadcasts
+    persist_broadcasters(response_hash[:broadcasters])
+    persist_broadcasts(response_hash[:broadcasts])
+  end
+
+  private
+
+  def self.persist_broadcasts(broadcasts)
+    broadcasts.each do |broadcast|
+      record             = Meerkat.new(map_broadcast_response(broadcast))
+      record.broadcaster = Broadcaster.find_by(external_id: broadcast["result"]["broadcaster"]["id"])
+      record.save
+    end
+  end
+
+  def self.persist_broadcasters(broadcasters)
+    broadcasters.each do |broadcaster|
+      Broadcaster.create(map_broadcaster_response(broadcaster))
+    end
+  end
+
+  def self.map_broadcast_response(response)
+    {
+      external_id:      response["result"]["id"],
+      cover:            response["result"]["cover"],
+      caption:          response["result"]["caption"],
+      latitude:         nil,
+      longitude:        nil,
+      location_string:  response["result"]["location"],
+      place_name:       response["result"]["place"],
+      watchers_count:   response["result"]["watchersCount"],
+      comments_count:   response["result"]["commentsCount"],
+      likes_count:      response["result"]["likesCount"],
+      restreams_count:  response["result"]["restreamsCount"],
+      cover_images:     (response["result"]["coverImages"] || []),
+      status:           response["result"]["status"],
+      end_time:         parse_end_time(response["result"]["endTime"]),
+      twitter_tweet_id: "#{response['result']['tweetId']}",
+      playlist_url:     response["followupActions"].try(:[], "playlist")
+    }
+  end
+
+  def self.map_broadcaster_response(response)
+    {
+      external_id:          response["result"]["info"]["id"],
+      username:             response["result"]["info"]["username"],
+      display_name:         response["result"]["info"]["displayName"],
+      avatar_url:           response["followupActions"]["profileImage"],
+      avatar_thumbnail_url: response["followupActions"]["profileThumbImage"],
+      profile_url:          nil,
+      twitter_user_id:      response["result"]["info"]["twitterId"],
+      privacy:              response["result"]["info"]["privacy"],
+      bio:                  response["result"]["info"]["bio"],
+      streams_count:        response["result"]["stats"]["streamsCount"],
+      following_count:      response["result"]["stats"]["followingCount"],
+      followers_count:      response["result"]["stats"]["followersCount"],
+      score:                response["result"]["stats"]["score"]
+    }
+  end
+
+  def self.parse_end_time(end_time)
+    end_time > 0 ? Time.at(end_time) : nil
+  end
+end
